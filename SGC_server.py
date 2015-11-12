ae313fe8cf1b9bbc65824e61b3a3823a0b596a71
David Roddick <dgroddick@gmail.com>
2015-11-12 23:38:18 +1100

    Tidy up response message

--- SGC_server.py
+++ SGC_server.py
@@ -2,6 +2,11 @@
 from socket import *
 from addresses import db
 
+NUM_CHEVRONS = 6
+
+user_data = []  # list for storing the whole address the users sends
+inp = None
+
 HOST = '127.0.0.1'
 PORT = 29877
 ADDR = (HOST,PORT)
@@ -11,32 +16,28 @@ serv = socket( AF_INET,SOCK_STREAM)
 serv.bind((ADDR))
 serv.listen(1)
 
-d = []
-inp = None
-
-
+# listen loop
 while True:
-    print "Listening"
 
     conn, addr = serv.accept()
 
-    print "Connected to ", ADDR
     conn.send("Welcome to StarGate Command ")
 
+    # collect user input
     while True:
 
-        # 6 symbols
-        if len(d) == 6:
+        # 6 symbols plus point of origin
+        if len(user_data) == NUM_CHEVRONS:
             break
 
-        # store the users input
+        # store the users input into the list
         inp = conn.recv(BUFSIZE)
-        d.append(int(inp))
+        user_data.append(int(inp))
 
     # compare the users list with the lists in the address database
-    for k, v in db.iteritems():
-        if cmp(d, v) == 0:
-            conn.send('%s MATCH - %s' % (k, d))
+    for key, value in db.iteritems():
+        if cmp(user_data, value) == 0:
+            conn.send('%s - ENCODED .... Connecting to:  %s' % (user_data, key))
 
 
 conn.close()
