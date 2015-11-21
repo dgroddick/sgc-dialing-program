4e7016afbdbf0560cd0213c1c74d0db745690b9b
David Roddick <dgroddick@gmail.com>
2015-11-22 10:03:59 +1100

    server class

--- SGC_server.py
+++ SGC_server.py
@@ -8,47 +8,58 @@ POINT_OF_ORIGIN = 1
 dialed_address = []  # list for storing the whole address the users sends
 inp = None
 
-HOST = '127.0.0.1'
-PORT = 29876
-ADDR = (HOST,PORT)
-BUFSIZE = 4096
 
-serv = socket(AF_INET,SOCK_STREAM)
-serv.bind((ADDR))
-serv.listen(1)
+class SGC_Server():
 
-listen = True
+    def __init__(self):
+        self.HOST = '127.0.0.1'
+        self.PORT = 29876
+        self.ADDR = (self.HOST, self.PORT)
+        self.BUFSIZE = 4096
+        self.serv = None
 
-# listen loop
-while listen:
+    def listen(self):
+        self.serv = socket(AF_INET,SOCK_STREAM)
+        self.serv.bind((self.ADDR))
+        self.serv.listen(1)
 
-    conn, addr = serv.accept()
-    print "Connected to client %s\n" % (str(addr))
+        listen = True
 
-    conn.send("Welcome to StarGate Command \n\n")
+        # listen loop
+        while listen:
 
-    # collect user input
-    while True:
+            conn, addr = self.serv.accept()
+            print "Connected to client %s\n" % (str(addr))
 
-        # 6 symbols plus point of origin
-        if len(dialed_address) == NUM_CHEVRONS:
-            break
-        else:
-            # store the users input into the list
-            inp = conn.recv(BUFSIZE)
-            dialed_address.append(int(inp))
-            print "%d ENCODED" % (int(inp))
-            conn.send("ENCODED\n")
+            conn.send("Welcome to StarGate Command \n\n")
 
-    # compare the users list with the lists in the address database
-    for key, value in db.iteritems():
-        if cmp(dialed_address, value) == 0:
-            print "%s - WORMHOLE ESTABLISHED" % (dialed_address)
-            print "\n .... Connecting to:  %s\n" % (key)
+            # collect user input
+            while True:
 
-            conn.send("%s - WORMHOLE ESTABLISHED" % (dialed_address))
-            conn.send("\n .... Connecting to:  %s\n" % (key))
-            listen = False
+                # 6 symbols plus point of origin
+                if len(dialed_address) == NUM_CHEVRONS:
+                    break
+                else:
+                    # store the users input into the list
+                    inp = conn.recv(self.BUFSIZE)
+                    dialed_address.append(int(inp))
+                    print "%d ENCODED" % (int(inp))
+                    conn.send("ENCODED\n")
+
+            # compare the users list with the lists in the address database
+            for key, value in db.iteritems():
+                if cmp(dialed_address, value) == 0:
+                    print "%s - WORMHOLE ESTABLISHED" % (dialed_address)
+                    print "\n .... Connecting to:  %s\n" % (key)
+
+                    conn.send("%s - WORMHOLE ESTABLISHED" % (dialed_address))
+                    conn.send("\n .... Connecting to:  %s\n" % (key))
+                    listen = False
 
 
-conn.close()
+        conn.close()
+
+if __name__ == '__main__':
+
+    srv = SGC_Server()
+    srv.listen()
