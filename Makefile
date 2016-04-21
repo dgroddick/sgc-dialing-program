a288c52c99e198345b89186121178f3d68495024
David Roddick <dgroddick@gmail.com>
2016-04-21 15:17:46 +1000

    added stargate image and close event

--- Makefile
+++ Makefile
@@ -1,8 +1,8 @@
 CC=gcc
-CFLAGS=-Wall -Werror -std=c99 
+CFLAGS=-Wall -Werror -g -std=c99 
 SRC=stargate.c
 TARGET=stargate
-LIBS=allegro-5 allegro_main-5
+LIBS=allegro-5 allegro_main-5 allegro_image-5 allegro_dialog-5
 
 
 stargate:
--- stargate.c
+++ stargate.c
@@ -1,28 +1,64 @@
 #include <stdio.h>
 #include <allegro5/allegro.h>
+#include <allegro5/allegro_image.h>
+#include <allegro5/allegro_native_dialog.h>
 
 int main(int argc, char *argv[])
 {
     ALLEGRO_DISPLAY *display = NULL;
+    ALLEGRO_BITMAP *image = NULL;
+    ALLEGRO_EVENT_QUEUE *event_queue = NULL;
 
-    if (!al_init()) {
-        fprintf(stderr, "Failed to initialise Allegro\n");
-        return -1;
+
+    if(!al_init()) {
+      al_show_native_message_box(display, "Error", "Error", "Failed to initialize allegro!", NULL, ALLEGRO_MESSAGEBOX_ERROR);
+      return 0;
     }
 
-    display = al_create_display(640, 480);
-    if (!display) {
-        fprintf(stderr, "Failed to create display\n");
-        return -1;
+    display = al_create_display(800,600);
+    if(!display) {
+      al_show_native_message_box(display, "Error", "Error", "Failed to initialize display!", NULL, ALLEGRO_MESSAGEBOX_ERROR);
+      return 0;
     }
 
-    al_clear_to_color(al_map_rgb(0, 0, 0));
+    event_queue = al_create_event_queue();
+    if(!event_queue) {
+      fprintf(stderr, "failed to create event_queue!\n");
+      al_destroy_display(display);
+      return -1;
+    }
 
-    al_flip_display();
+    al_register_event_source(event_queue, al_get_display_event_source(display));
 
-    al_rest(10.0);
+    if(!al_init_image_addon()) {
+      al_show_native_message_box(display, "Error", "Error", "Failed to initialize al_init_image_addon!", NULL, ALLEGRO_MESSAGEBOX_ERROR);
+      return 0;
+    }
+
+    image = al_load_bitmap("stargate.png");
+    if(!image) {
+      al_show_native_message_box(display, "Error", "Error", "Failed to load image!", NULL, ALLEGRO_MESSAGEBOX_ERROR);
+      al_destroy_display(display);
+      return 0;
+    }
+
+    while(1) {
+      ALLEGRO_EVENT ev;
+      ALLEGRO_TIMEOUT timeout;
+      al_init_timeout(&timeout, 0.06);
+
+      bool get_event = al_wait_for_event_until(event_queue, &ev, &timeout);
+
+      if(get_event && ev.type == ALLEGRO_EVENT_DISPLAY_CLOSE) {
+         break;
+      }
+      al_draw_bitmap(image, 10, 10,0);
+      al_flip_display();
+    }
 
     al_destroy_display(display);
+    al_destroy_bitmap(image);
+    al_destroy_event_queue(event_queue);
 
     return 0;
 }
